d3d2442ee347eaa4a10902a515894a747121c150
Matthew Rozendaal <rozendaal.matthew@gmail.com>
2022-11-07 21:48:47 -0600

    Started thinking about how I want to structure this project and general thought process on how I want the user flow to be.

--- LeanTechniquesPhotoAlbumTechnicalShowcase/Program.cs
+++ LeanTechniquesPhotoAlbumTechnicalShowcase/Program.cs
@@ -11,7 +11,18 @@ namespace LeanTechniquesPhotoAlbumTechnicalShowcase
         static void Main(string[] args)
         {
 
-
+            /*TODO: Thoughts
+             * Need to ask if they already know the album they would like
+             * 1) Look up by name
+             * 2) Look up by id
+             * 3) Show options
+             * 4) Show all albums and photos
+             * 
+             * GetAllPhotosFromURL
+             *  OrderPhotos into albums
+             *  OutputAlbums (in order)
+             *  Output Photos (in order)            
+             */
         }
     }
 }
